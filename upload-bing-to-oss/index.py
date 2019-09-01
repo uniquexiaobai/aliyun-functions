fc4aae445e1db0800807dc0b1576d2b2dc3fcab9
uniquexiaobai <unique_xiaobai@qq.com>
2019-09-01 15:44:41 +0800

    upload-bing-to-oss

---
+++ upload-bing-to-oss/index.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import logging
+import requests
+import oss2
+
+fetchUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&n=1&mkt=zh-CN&idx=0'
+baseUrl = 'https://cn.bing.com'
+
+auth = oss2.Auth('<yourAccessKeyId>', '<yourAccessKeySecret>')
+bucket = oss2.Bucket(auth, '<yourEndpoint>', '<yourBucketName>')
+
+def fetch():
+	res = requests.get(fetchUrl).json()
+	data = res['images'][0]
+	url = baseUrl + data['url']
+	key = data['enddate']
+	return key, url
+
+def handler(event, context):
+    logger = logging.getLogger()
+    key, url = fetch()
+    logger.info(key, url)
+    bucket.put_object(key + '.jpeg', requests.get(url))
+    return key, url
